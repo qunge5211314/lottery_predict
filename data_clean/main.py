1edd205702afa7785dbfb597dcf7acfe017605ba
JustinHan <>
2021-03-12 09:02:31 +0800

    no message

---
+++ data_clean/main.py
@@ -0,0 +1,6 @@
+#!python3
+# -*- coding: utf-8 -*-
+# Author: JustinHan
+# Date: 2021-03-12
+# Introduce: 大乐透数据清理
+# Dependence
--- data_collect/main.py
+++ data_collect/main.py
@@ -7,6 +7,7 @@
 import requests
 import json
 import pandas as pd
+import time
 
 lottery_data_url = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry"
 max_page = 10000000000000
@@ -17,6 +18,7 @@ columns = ["series", "date", "first", "second", "third", "fourth", "fifth", "six
 def get_data():
     data = []
     for pageNo in range(1, max_page):
+        time.sleep(2)
         res = requests.get(lottery_data_url, params={"gameNo": 85,
                                                      "provinceId": 0,
                                                      "pageSize": 30,
@@ -34,16 +36,35 @@ def get_data():
                          "fifth": fifth,
                          "sixth": sixth,
                          "seventh": seventh,
-                         "date": info.get("lotteryDrawTime"),
-                         "draw_no": info.get("lotteryDrawNum")})
+                         "date": info.get("lotteryDrawTime")})
+            # "draw_no": info.get("lotteryDrawNum")
     return data
 
 
 # 保存数据
 def save(data):
-    date, draw_no, = []
+    date, draw_no, first, second, third, fourth, fifth, sixth, seventh = [], [], [], [], [], [], [], [], []
     for info in data:
-        pass
+        date.append(info.get("date"))
+        # draw_no.append(info.get("draw_no"))
+        first.append(info.get("first"))
+        second.append(info.get("second"))
+        third.append(info.get("third"))
+        fourth.append(info.get("fourth"))
+        fifth.append(info.get("fifth"))
+        sixth.append(info.get("sixth"))
+        seventh.append(info.get("seventh"))
+    dic = {"date": date,
+           # "draw_no": draw_no,
+           "first": first,
+           "second": second,
+           "third": third,
+           "fourth": fourth,
+           "fifth": fifth,
+           "sixth": sixth,
+           "seventh": seventh}
+    df = pd.DataFrame(dic)
+    df.to_csv("../raw_data.csv")
 
 
 if __name__ == '__main__':
